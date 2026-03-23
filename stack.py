94ccb4fcdafa2e5f754afcbfc3a482c1518703d5
ravish164 <yadavravish164@gmail.com>
2026-03-23 16:34:47 +0530

    this is the code of stack

---
+++ stack.py
@@ -0,0 +1,9 @@
+# Stack Question
+stack = []
+stack.append("Book 1")
+stack.append("Book 2")
+stack.append("Book 3")
+print("Stack:", stack)
+top_book = stack.pop()
+print("Top book:", top_book)
+print("Remaining:", stack)
